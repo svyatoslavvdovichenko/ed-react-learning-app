480a94e692651a2f929f0ac8b0cda33062473e6d
Svyatoslav <vdovichenko.fortech@gmail.com>
2022-10-28 16:52:24 +0300

    feat: Added test for NewTask page
    git push origin test-new-task -f

---
+++ src/tests/pages/NewTask.test.tsx
@@ -0,0 +1,72 @@
+import { render } from '@testing-library/react'
+import { NewTask } from '../../pages/NewTask'
+
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom')
+
+  return {
+    ...originalModule,
+    Link: () => <div />,
+    useLocation: () => ({
+      location: {
+        search: 'search',
+        pathname: 'pathname',
+      },
+    })
+  }
+})
+
+jest.mock('../../hooks/useReferences', () => ({
+  useReferences: () => ({
+    technologies: [
+      {
+        id: 123,
+        title: 'adaptive',
+      },
+    ],
+    specialization: [
+      {
+        id: 1,
+        title: 'frontend',
+      },
+    ],
+  }),
+}))
+
+jest.mock('../../hooks/useUser', () => ({
+  useUser: () => ({
+    email: 'string',
+    first_name: 'string',
+    id: 213,
+    is_admin: false,
+    last_name: 'string',
+    completed_tasks: [],
+    active_tasks: [],
+  }),
+}))
+
+jest.mock('../../hooks/useAuth', () => ({
+  useAuth: () => ({
+    onLogout: () => {},
+    onLogin: () => {},
+  }),
+}))
+
+describe('NewTask', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      value: () => {
+        return {
+          matches: false,
+          addListener: () => {},
+          removeListener: () => {},
+        }
+      },
+    })
+  })
+
+  test('NewTask', () => {
+    const { container } = render(<NewTask />)
+    expect(container).toBeTruthy()
+  })
+})
