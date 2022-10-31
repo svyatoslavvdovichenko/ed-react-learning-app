fccb3a30b13426518a8dcea3ed4354d10e3303fb
Svyatoslav <vdovichenko.fortech@gmail.com>
2022-10-31 09:48:35 +0300

    feat: user page

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,4 +1,4 @@
-import { Card } from 'antd'
+import { Button, Card } from 'antd'
 import { Col, Row } from 'antd/lib/grid'
 import Text from 'antd/lib/typography/Text'
 import { FC } from 'react'
@@ -6,40 +6,80 @@ import { Layout } from '../components/Layout'
 import { StyledText } from '../components/common/StyledComponents'
 import { useUser } from '../hooks/useUser'
 import { TaskItem } from '../components/dashboard/TaskItem'
+import { EditOutlined } from '@ant-design/icons'
 
 export const UserProfile: FC = () => {
-  const { user } = useUser()
-
-  console.log('user', user)
+  const { user } = useUser();
 
   return (
     <Layout>
       <Row justify="center">
         <Col flex="0 1 970px">
           <Card>
-            <Row>
+            <Row style={{display: "flex", justifyContent: "space-between"}}>
               <StyledText>
                 {user?.first_name} {user?.last_name}
               </StyledText>
+
+              <Button
+                  type="primary"
+                  ghost
+                  style={{ height: 40, marginRight: 20 }}
+                >
+                  <EditOutlined />
+                    Редактировать профиль
+                </Button>
             </Row>
 
             <Row>
-              <Text>{user?.email}</Text>
+              <Text style={{marginTop: "-10px"}} disabled>{user?.email}</Text>
             </Row>
 
+            <Row style={{marginTop: "30px"}}>
+              <StyledText>
+                Статистика пользователя
+              </StyledText>
+            </Row>
+
+            <Row style={{ width: "35%", display: "flex", borderBottom: "1px solid #EEEEEE", flexWrap: "nowrap"}}>
+              <StyledText style={{marginTop: "20px", paddingBottom: "10px", fontSize: "14px", width: "100%"}}>
+                {`Заданий выполнено `}
+              </StyledText>
+
+              <StyledText style={{marginTop: "20px", fontSize: "14px"}}>
+                {user?.completed_tasks.length}
+              </StyledText>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row justify="center" style={{marginTop: "30px"}}>
+        <Col flex="0 1 970px">
+          <Card>
             <StyledText>Активные задания</StyledText>
 
             <Row>
-              {user?.active_tasks.map((task) => (
+              {user?.active_tasks.length !== 0? user?.active_tasks.map((task) => (
                 <TaskItem key={task.id} task={task} />
-              ))}
+              )) : (
+                <StyledText style={{marginTop: "10px", fontSize: "14px"}}>Нет активных заданий</StyledText>
+              )}
             </Row>
+          </Card>
+        </Col>
+      </Row>
 
+      <Row justify="center" style={{marginTop: "30px"}}>
+        <Col flex="0 1 970px">
+          <Card>
             <StyledText>Выполненные задания</StyledText>
 
-            {user?.completed_tasks.map((task) => (
+            {user?.completed_tasks.length !== 0? user?.completed_tasks.map((task) => (
               <TaskItem key={task.id} task={task} />
-            ))}
+            )) : (
+              <p style={{marginTop: "10px"}}>Нет выполненных заданий</p>
+            )}
           </Card>
         </Col>
       </Row>
